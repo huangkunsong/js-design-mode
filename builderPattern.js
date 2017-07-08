d25eb631520d3a2e23edb31c1b4b403795ac4116
huangkunsong <huang_kunsong@sina.cn>
2017-07-08 23:35:43 +0800

    Factory mode implement

---
+++ builderPattern.js
@@ -0,0 +1,51 @@
+/**
+ * Created by rookie on 2017/7/8.
+ * All copyright belongs to rookie.
+ */
+/**
+ * 建造者模式：将一个复杂对象的构建层和表示层分离,通用的建造过程采用不同的表现。
+ *  建造者模式和工厂模式都是用于创建对象实例。
+ *  但工厂模式关注的是最后得到的对象,不关注创建的过程。
+ *  建造者模式则更关注建造的细节,同一个建造方法根据参数的不同可以得到不同的表现。
+ *
+ */
+const BuilderResume = (function (){
+    
+    function Person (name, hobby){
+        this.name = name;
+        this.hobby = hobby;
+    }
+    
+    Person.prototype.getHobby = function () {
+        return this.hobby;
+    };
+    
+    
+    function Work (work){
+        switch (work){
+            case "code" :
+                this.work = "程序员";
+                this.workDescript = "天天敲代码";
+                break;
+            case "student":
+                this.work = "学生";
+                this.workDescript = "天天上课";
+                break;
+            default:
+                throw new Error("没有该职业");
+        }
+    }
+    
+    Work.prototype.changeWorkDesc = function (desc) {
+        this.workDescript = desc;
+    };
+    
+    return function (name, hobby, work){
+        const _person = new Person(name, hobby);
+        _person.work = new Work(work);
+        return _person;
+    };
+}());
+
+
+var a = BuilderResume("张三", "游泳", "code");
--- factoryMethod.js
+++ factoryMethod.js
@@ -7,11 +7,17 @@
  * 工厂类通过对实例类的抽象使其主要负责创建不同的类实例。
  * 将具体的类型写到工厂方法的原型中。
  * 优点：后期添加类,只需要在原型中添加,不需要改动工厂方法。
+ *
+ * @example
+ *      new AnimalFactory("Dog");  //得到Dog实例
  */
 const AnimalFactory = function (type){
     //安全模式类,确保该类是new调用,不是直接使用
     if (this instanceof AnimalFactory){
-        return new this[type]();
+        if (this.hasOwnProperty(AnimalFactory.prototype[type])){
+            return new this[type]();
+        }
+        throw Error(`Factory no ${type} class`);
     } else {
         return new AnimalFactory(type);
     }
@@ -25,3 +31,26 @@ Object.assign(AnimalFactory.prototype, {
         this.type = "Cat";
     },
 });
+/**
+ * 写法2
+ * 不用每次都实例化工厂
+ * @example
+ *      AnimalFactory2("Dog");
+ */
+const AnimalFactory2 = (function (type){
+    var Animal = {
+        Dog : () => {
+            this.type = "Dog";
+        },
+        Cat : () => {
+            this.type = "Cat";
+        },
+    };
+   
+    return function (type) {
+        if (Animal.hasOwnProperty(type)){
+            return new Animal[type]();
+        }
+        throw Error(`Factory no ${type} class`);
+    };
+}());
